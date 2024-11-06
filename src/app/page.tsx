ee144bd9fe5a44a1f9676b60980f52a8582b28a4
Habsyi2512 <githabsyi@gmail.com>
2024-11-07 01:43:16 +0700

    "Remove XMarkIcon component, update globals.css, and modify page.tsx"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { XMarkIcon } from "@/components/XMarkIcon";
+import { XMarkIcon } from "@/components/icons/XMarkIcon";
 
 export default function Home() {
   const [state, setState] = useState<boolean>(false);
@@ -26,15 +26,15 @@ export default function Home() {
             className="w-full h-screen fixed flex items-center justify-center top-0 left-0 p-2 bg-black/30 backdrop-blur-sm"
           >
             <motion.div
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
               className="w-full max-w-3xl bg-black text-white rounded-2xl h-[500px] relative"
             >
               <button
                 onClick={() => setState(false)}
-                className="group absolute hover:bg-white/50 p-4 z-10 top-5 right-5"
+                className="group absolute hover:bg-white/50 transition-colors duration-200 p-2 rounded-full z-10 top-5 right-5"
               >
-                close
+                <XMarkIcon className="size-6" />
               </button>
             </motion.div>
           </motion.div>
